1888b380816b3365ba7e22ee65698e343e99ead3
mcianni <mcianni@gmail.com>
2015-09-05 00:27:39 -0400

    Initial commit.

--- Gyrogo/ViewController.h
+++ Gyrogo/ViewController.h
@@ -6,10 +6,19 @@
 //  Copyright (c) 2015 CMPLXLABS. All rights reserved.
 //
 
+#define RADIANS_TO_DEGREES(radians) ((radians) * (180.0 / M_PI))
+
 #import <UIKit/UIKit.h>
+@import CoreMotion;
+@import SceneKit;
 
 @interface ViewController : UIViewController
 
+@property (strong, nonatomic) CMMotionManager *motionManager;
+@property (strong, nonatomic) IBOutlet UILabel *yawLabel;
+@property (strong, nonatomic) IBOutlet UILabel *rollLabel;
+@property (strong, nonatomic) IBOutlet UILabel *pitchLabel;
+@property (strong, nonatomic) IBOutlet SCNView *scene;
 
 @end
 
--- Gyrogo/ViewController.m
+++ Gyrogo/ViewController.m
@@ -16,12 +16,55 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    SCNScene *scn = [[SCNScene alloc] init];
+    SCNBox *box = [SCNBox boxWithWidth:50.0 height:8.0 length:120.0 chamferRadius:1.0];
+    box.firstMaterial.diffuse.contents = [UIColor orangeColor];
+    SCNNode *node = [SCNNode nodeWithGeometry:box];
+    [scn.rootNode addChildNode:node];
+    _scene.scene = scn;
+    [_scene setBackgroundColor:[UIColor clearColor]];
+    [_scene setAutoenablesDefaultLighting:YES];
+    [_scene setAllowsCameraControl:YES];
+    
+    
+    
+    _motionManager = [[CMMotionManager alloc] init];
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    __weak typeof(self) weakSelf = self;
+    [_motionManager startDeviceMotionUpdatesToQueue:queue withHandler:^(CMDeviceMotion *motion, NSError *error) {
+        [weakSelf updateMotion:motion];
+    }];
+}
+
+- (void)updateMotion:(CMDeviceMotion *)motion
+{
+    [self performSelectorOnMainThread:@selector(updateLabels:) withObject:motion waitUntilDone:NO];
+}
+
+- (void)updateLabels:(CMDeviceMotion *)motion
+{
+    [_yawLabel   setText:[NSString stringWithFormat:@"%0.1f˚", RADIANS_TO_DEGREES(motion.attitude.yaw)]];
+    [_rollLabel  setText:[NSString stringWithFormat:@"%0.1f˚", RADIANS_TO_DEGREES(motion.attitude.roll)]];
+    [_pitchLabel setText:[NSString stringWithFormat:@"%0.1f˚", RADIANS_TO_DEGREES(motion.attitude.pitch)]];
+    
+}
+
+- (void)orientationChanged:(NSNotification *)notification
+{
+    
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    [[UIDevice currentDevice] endGeneratingDeviceOrientationNotifications];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 @end
